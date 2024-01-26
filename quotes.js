e669d9a71722397a637bfa20012a772e68834143
ELondonJ <ljoseph@pdx.edu>
2024-01-26 13:00:28 -0800

    remove type.fit from authors

--- quotes.js
+++ quotes.js
@@ -1,16 +1,16 @@
-const url = "https://type.fit/api/quotes";
-let default_message = "Thanks for stopping by!";
+const url = 'https://type.fit/api/quotes';
+let default_message = 'Thanks for stopping by!';
 let body = document.body;
 
 function createDomElement(data) {
   console.log(data);
-  let section = document.createElement("section");
-  section.className = "bg-dark p-3 text-light d-flex";
-  section.id = "quote";
+  let section = document.createElement('section');
+  section.className = 'bg-dark p-3 text-light d-flex mt-1';
+  section.id = 'quote';
   body.appendChild(section);
-  let quote = document.createElement("h6");
-  quote.className = "m-auto";
-  quote.id = "quote-header";
+  let quote = document.createElement('h6');
+  quote.className = 'm-auto';
+  quote.id = 'quote-header';
   quote.textContent = data;
   section.appendChild(quote);
 }
@@ -18,11 +18,10 @@ const fetchData = async function (url) {
   try {
     let response = await fetch(url);
     let data = await response.json();
-    console.log(data);
-    console.log("length", data.length);
     let rand = Math.floor(Math.random() * data.length);
-    console.log("rand", rand);
-    let text = data[rand].text + " - " + data[rand].author;
+    let author = data[rand].author;
+    author = author.replace(', type.fit', '');
+    let text = data[rand].text + ' - ' + author;
     createDomElement(text);
   } catch (error) {
     createDomElement(default_message);
